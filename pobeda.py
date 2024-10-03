3502fb09b5ac48128b4d0d499454a6295fd43c4a
Maksim Vysochanskii <hot-zinc@yandex.ru>
2024-10-03 14:52:32 +0600

    MVP автоматизации выгрузки

--- pobeda.py
+++ pobeda.py
@@ -1,27 +1,43 @@
+
 import pyautogui, os, keyboard
-import openpyxl
 from time import sleep
-loads_coordinate = [(30, 120), (100, 135), (450, 90), (500, 170), (1895, 15)]
-# запуск exe файла
-os.startfile("C:/OEMZ/Production manager/ProductionManager.exe")
-sleep(3)
-for i in range(4):
+
+from extract import ext
+
+loads_coordinate = [(25, 120), (100, 135), (450, 90), (500, 170), (1895, 15)]
+
+def run():
+    # запуск exe файла
+    os.startfile("C:/OEMZ/Production manager/ProductionManager.exe")
+    sleep(3)
+
+    # авторизация в победе
+    for i in range(3):
+        keyboard.press("enter")
+        sleep(3)
+
+    # выгрузка excel файла
+    sleep(1)
+    for i in loads_coordinate:
+        pyautogui.click(i)
+        sleep(3)
+
+    # сохранение файла
+    sleep(4)
+    keyboard.press("ctrl + s")
+    sleep(3)
+
+    for i in range(4):
+        keyboard.press("enter")
+        sleep(2)
+
+    keyboard.press("left")
+    sleep(1)
     keyboard.press("enter")
     sleep(2)
 
-# выгрузка excel файла
-for i in loads_coordinate:
-    pyautogui.click(i)
-    sleep(5)
+    ext()
 
-# сохранение файла
-keyboard.press("ctrl + s")
-sleep(3)
 
-for i in range(4):
-    keyboard.press("enter")
-    sleep(2)
-
-keyboard.press("left")
-sleep(1)
-keyboard.press("enter")
+if __name__ == "__main__":
+    run()
